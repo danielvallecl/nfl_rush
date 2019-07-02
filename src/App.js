f43bd73b862b75502c941c59250105ae138b9714
Daniel Valle <daniel.vallecl@gmail.com>
2019-07-02 09:36:36 -0400

    Added React Router

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// ======= COMPONENTS =======
+
+import './components/404';
+import './components/About';
+import './components/Home';
+import './components/Navbar';
+import './components/Players';
+import './components/Search';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
