3d2325be16d13709a266f2cf6cdee9b8179b58a9
Daniel Valle <daniel.vallecl@gmail.com>
2019-07-02 11:08:33 -0400

    Added Dashboard folder and component files

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,27 @@
  * @version: 1.0
  */
 
+ import React, { Component } from "react";
 
-import React, { Component } from 'react';
+ // ======== COMPONENTS ======== //
 
-class Home extends Component {
-  render() {
-    return(
-      <h1>I am Home</h1>
-    )
-  }
-}
+ import DashHome from "./dashboard/DashHome";
+ import DashPlayer from "./dashboard/DashPlayer";
 
-export default Home;
+// ======== CSS ======== //
+
+ import "./Home.scss";
+
+ class Home extends Component {
+
+   render() {
+
+     return (
+       <React.Fragment>
+           <DashboardHome />
+       </React.Fragment>
+     );
+   }
+ }
+
+ export default Home;
