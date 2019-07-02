1a058add12a60bde61a16757e58ae682b0cb9df1
Daniel Valle <daniel.vallecl@gmail.com>
2019-07-02 16:30:34 -0400

    Fixed the navbar bug over the navbar-brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -44,7 +44,7 @@ class Navbar extends Component {
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse justify-content-end nav-col" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <Link to="/teams" className="nav-link btn btn-link">Teams
             </Link>
             <Link to="/players" className="nav-link btn btn-link">Players
