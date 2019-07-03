0d6ff006c1f283f47cc8b29884a27460365027d7
Daniel Valle <daniel.vallecl@gmail.com>
2019-07-03 19:28:45 -0400

    Added margin to inputs

--- src/components/Search.js
+++ src/components/Search.js
@@ -101,6 +101,50 @@
        <>
        <div className= "grid-container">
          <div className="search-div">
+           {localStorage.getItem('count') !== '0' ?
+           <table className="table">
+             <thead>
+               <tr>
+                 <th>Name</th>
+                 <th>Team</th>
+                 <th>Pos</th>
+                 <th>Att</th>
+                 <th>Att/G</th>
+                 <th>Yds</th>
+                 <th>Avg</th>
+                 <th>Yds/G</th>
+                 <th>TD</th>
+                 <th>Lng</th>
+                 <th>1st</th>
+                 <th>1st%</th>
+                 <th>20+</th>
+                 <th>40+</th>
+                 <th>FUM</th>
+               </tr>
+             </thead>
+             <tbody>
+               <tr>
+                 <td>{ data[localStorage.getItem('count')]['Player'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['Team'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['Pos'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['Att'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['Att/G'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['Yds'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['Avg'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['Yds/G'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['TD'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['Lng'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['1st'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['1st%'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['20+'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['40+'] }</td>
+                 <td>{ data[localStorage.getItem('count')]['FUM'] }</td>
+               </tr>
+             </tbody>
+           </table>
+
+           : null
+           }
          <form action="#">
            <select className="search-results" size={localStorage.getItem('numb_of_items')} id="player" onChange={this.handleOnSelect}>
            {JSON.parse(localStorage.getItem('players_array')).map((player, key) => {
@@ -109,52 +153,8 @@
            </select>
          </form>
          <div id="list">
-         {localStorage.getItem('count') !== '0' ?
-         <table className="table">
-           <thead>
-             <tr>
-               <th>Name</th>
-               <th>Team</th>
-               <th>Pos</th>
-               <th>Att</th>
-               <th>Att/G</th>
-               <th>Yds</th>
-               <th>Avg</th>
-               <th>Yds/G</th>
-               <th>TD</th>
-               <th>Lng</th>
-               <th>1st</th>
-               <th>1st%</th>
-               <th>20+</th>
-               <th>40+</th>
-               <th>FUM</th>
-             </tr>
-           </thead>
-           <tbody>
-             <tr>
-               <td>{ data[localStorage.getItem('count')]['Player'] }</td>
-               <td>{ data[localStorage.getItem('count')]['Team'] }</td>
-               <td>{ data[localStorage.getItem('count')]['Pos'] }</td>
-               <td>{ data[localStorage.getItem('count')]['Att'] }</td>
-               <td>{ data[localStorage.getItem('count')]['Att/G'] }</td>
-               <td>{ data[localStorage.getItem('count')]['Yds'] }</td>
-               <td>{ data[localStorage.getItem('count')]['Avg'] }</td>
-               <td>{ data[localStorage.getItem('count')]['Yds/G'] }</td>
-               <td>{ data[localStorage.getItem('count')]['TD'] }</td>
-               <td>{ data[localStorage.getItem('count')]['Lng'] }</td>
-               <td>{ data[localStorage.getItem('count')]['1st'] }</td>
-               <td>{ data[localStorage.getItem('count')]['1st%'] }</td>
-               <td>{ data[localStorage.getItem('count')]['20+'] }</td>
-               <td>{ data[localStorage.getItem('count')]['40+'] }</td>
-               <td>{ data[localStorage.getItem('count')]['FUM'] }</td>
-             </tr>
-           </tbody>
-         </table>
-
-         : null
-         }
          </div>
-         <button onClick={this.handleOnSubmit}>
+         <button className="page-link" onClick={this.handleOnSubmit}>
          Return
          </button>
          </div>
